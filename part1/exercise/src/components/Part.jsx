cfb39db061dcc95f3e44ff2c555b372f41d95c9c
Dav.O <davolufuwa@gmail.com>
2023-10-16 13:31:06 +0100

    started completed part 1.1-1.3

---
+++ part1/exercise/src/components/Part.jsx
@@ -0,0 +1,7 @@
+const Part = () => {
+  return (
+    <div>Part</div>
+  )
+}
+
+export default Part
---
+++ part1/exercise/src/components/Total.jsx
@@ -0,0 +1,7 @@
+const Total = () => {
+  return (
+    <div>Total</div>
+  )
+}
+
+export default Total
