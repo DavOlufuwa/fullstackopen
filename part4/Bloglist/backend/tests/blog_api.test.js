62d4a17400b91de904bd1a5f01447e4e5142d1da
Dav.O <davolufuwa@gmail.com>
2023-10-26 15:39:36 +0100

    add the 'express-async-errors'

--- part4/Bloglist/backend/tests/blog_api.test.js
+++ part4/Bloglist/backend/tests/blog_api.test.js
@@ -1,58 +1,44 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const helper = require('./test_helper')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialBlogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  }
-]
 
   beforeEach(async () => {
     await Blog.deleteMany({})
     
-    let blogObject = new Blog(initialBlogs[0])
+    let blogObject = new Blog(helper.initialBlogs[0])
 
     await blogObject.save()
 
-    blogObject = new Blog(initialBlogs[1])
+    blogObject = new Blog(helper.initialBlogs[1])
 
     await blogObject.save()
   }, 1000000)
 
 
 
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-  })
+  // test('blogs are returned as json', async () => {
+  //   await api
+  //     .get('/api/blogs')
+  //     .expect(200)
+  //     .expect('Content-Type', /application\/json/)
+  // })
 
-  test('there are two blogs returned', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length)
-  })
+  // test('there are two blogs returned', async () => {
+  //   const response = await api.get('/api/blogs')
+  //   expect(response.body).toHaveLength(helper.initialBlogs.length)
+  // })
 
-  test('a specific note is within the returned notes', async () => {
-    const response = await api.get('/api/blogs')
+  // test('a specific note is within the returned notes', async () => {
+  //   const response = await api.get('/api/blogs')
     
-    const contents = response.body.map(r => r.title)
+  //   const contents = response.body.map(r => r.title)
 
-    expect(contents).toContain('React patterns')
-  })
+  //   expect(contents).toContain('React patterns')
+  // })
 
 
 
@@ -69,15 +55,29 @@ const initialBlogs = [
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const response = await api.get('/api/blogs')
+    const blogsAtEnd = await helper.blogsInDb()
 
-    const contents = response.body.map(r => r.content)
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    const contents = blogsAtEnd.map(b => b.title)
+    expect(contents).toContain('test blog A')
+  })
 
-    expect(contents).toContain(
-      'test blog A'
-    )
+
+  test('a blog without contents is not added', async () => {
+    const newBlog = {
+      author: 'test author A',
+      url: 'http://testurl.com',
+      likes: 5
+    }
+
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
   })
 
   afterAll(async () => {
