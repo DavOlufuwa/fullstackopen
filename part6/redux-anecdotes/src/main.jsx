3d9385038b5cf05a348c55c7a64299d022032466
Dav.O <davolufuwa@gmail.com>
2023-11-08 17:38:37 +0100

    started using redux with a backend

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,25 +1,23 @@
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-import notificationReducer from './reducers/notificationReducer'
-import { configureStore } from '@reduxjs/toolkit'
-
-
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./services/App";
+import anecdoteReducer from "./reducers/anecdoteReducer";
+import filterReducer from "./reducers/filterReducer";
+import notificationReducer from "./reducers/notificationReducer";
+import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
     filter: filterReducer,
-    notification: notificationReducer
-  }
-}
-)
+    notification: notificationReducer,
+  },
+});
 
+// fetch all anecdotes
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
